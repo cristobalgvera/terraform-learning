4b34b131f9a766641aee2daf22050b4d01a1e97d
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-12-03 23:16:42 -0300

    feat(s3): apply base configuration

--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "environment" {
   description = "Name of the environment"
   default     = "learning"
 }
+
+variable "s3_bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket"
+}
